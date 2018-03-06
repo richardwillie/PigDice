d740cb8153e8fbd56cb9bba0e5100d1c4505155a
richard willie <richardwillie.w@gmail.com>
2018-03-06 07:56:53 +0300

    add alert prompts for both players

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function Player(name, points) {
+  this.name = name;
+  this.points = points;
+
+
+  var player1 = new player();
+  var player2 = new player();
+
+  var score = 0;
+  var score = 0;
